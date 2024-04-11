d3989413057bdc26c7a22ee50fdda629151b4316
Animeshdj <animeshstick.567@gmail.com>
2024-04-11 17:50:09 +0530

    t

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -3,12 +3,14 @@ import "./body.css";
 import SectionA from "./SectionA";
 import SectionB from "./SectionB";
 import SectionC from "./SectionC";
+import SectionD from "./SectionD";
 const Body = () => {
   return (
     <main>
       <SectionA />
       <SectionB />
       <SectionC />
+      <SectionD />
     </main>
   );
 };
---
+++ src/components/SectionD.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+
+const SectionD = () => {
+  return (
+    <div className="section-4">
+      <div className="section-4-container">
+        <div className="section-4-content">
+          <h3>How we Move</h3>
+          <div>
+            <p>
+              Manuport Logistics offers a different approach to shipping, guided
+              by creative thinking. Ship, barge, plane, train, truck, or a
+              combination. For you, we move mountains.
+            </p>
+            <a href="/" className="sec3-btn">
+              <span>Discover our services</span>
+              <div className="sec3-btn-btn">
+                <i className="btn-icon">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                  >
+                    <path
+                      fill="currentColor"
+                      fillRule="evenodd"
+                      d="M.15 12a.6.6 0 0 1 .6-.6h22.5a.6.6 0 0 1 0 1.2H.75a.6.6 0 0 1-.6-.6Z"
+                      clipRule="evenodd"
+                    ></path>
+                    <path
+                      fill="currentColor"
+                      fillRule="evenodd"
+                      d="M12.326 1.076a.6.6 0 0 1 .848 0l10.5 10.5a.6.6 0 0 1 0 .848l-10.5 10.5a.6.6 0 0 1-.848-.848L22.4 12 12.326 1.924a.6.6 0 0 1 0-.848Z"
+                      clipRule="evenodd"
+                    ></path>
+                  </svg>
+                </i>
+              </div>
+            </a>
+          </div>
+        </div>
+        <div className="section-4-graphic">
+          <div className="section-4-vidwrapper"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SectionD;
